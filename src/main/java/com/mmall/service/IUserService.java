361b9ad002c35d72958ee770f6e88cbfbdd0b152
carlcs <carlcs1994@gmail.com>
2019-03-12 12:36:34 +0800

    6-4

---
+++ src/main/java/com/mmall/service/IUserService.java
@@ -0,0 +1,15 @@
+package com.mmall.service;
+
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+
+/**
+ * Created by liu_changshi on 2019/3/11.
+ */
+public interface IUserService {
+    ServerResponse<User> login(String username, String password);
+    ServerResponse<String> register(User user);
+    ServerResponse<String> checkValid(String str,String type);
+    ServerResponse selectQuestion(String username);
+    ServerResponse<String> checkAnswer(String username,String question,String answer);
+}
